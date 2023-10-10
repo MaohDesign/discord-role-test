c7e0ac19238f9ee69b765393d86b78afcaa29a95
Maoh <maoh.design@gmail.com>
2023-10-10 16:36:53 +0900

    Add connect wallet

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,34 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import type { AppProps } from "next/app";
+import { createPublicClient, http } from "viem";
+import { WagmiConfig, createConfig, mainnet } from "wagmi";
+import { InjectedConnector } from "wagmi/connectors/injected";
+import { MetaMaskConnector } from "wagmi/connectors/metaMask";
+
+const config = createConfig({
+  autoConnect: true,
+  publicClient: createPublicClient({
+    chain: mainnet,
+    transport: http(),
+  }),
+  connectors: [
+    new MetaMaskConnector({
+      chains: [mainnet],
+      options: {
+        shimDisconnect: true,
+        UNSTABLE_shimOnConnectSelectAccount: true,
+      },
+    }),
+  ],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <WagmiConfig config={config}>
+        <Component {...pageProps} />
+      </WagmiConfig>
+    </ChakraProvider>
+  );
 }
